a67110fed4c2cb7047f64604e1d3740e75e9191d
khaled ghonem <KhaledGhonem724@gmail.com>
2023-12-28 23:57:50 +0200

    trainings renaming

---
+++ Trainings.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Level one</title>
+    <link rel="stylesheet" href="css/Trainings_Page.css">
+</head>
+
+<body class="TP_Body">
+    <div class="TP_Tiltle">
+        ICPC Trainings levels
+    </div>
+    <div class="TP_Page">
+        <div class="TP_Card">
+            <div class="TP_Card_Upper">
+                <p class="TP_Card_Title">
+                    problem solving training level one
+                </p>
+            </div>
+            <div class="TP_Card_Lower">
+                <span class="TP_Card_Info">
+                    <span class="TP_Card_Topics">
+                        <p>Topics</p>
+                        9999
+                    </span>
+                    <span class="TP_Card_Problems">
+                        <p>Problems</p>
+                        9999
+                    </span>
+                </span>
+                <span class="TP_Card_Enroll">
+                    <a href="contest.html" target="_blank" class="TP_Card_Button">
+                        <button type="submit" class="TP_Card_Button">Join</button>
+                    </a>
+                </span>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
