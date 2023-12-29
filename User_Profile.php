00ff5c33818105f955b5030e3c4f662a323a8554
AhmedGhanem50 <ahmedghanemcs@gmail.com>
2023-12-29 18:14:12 +0200

    بسم الله الرحمن الرحيم

--- User_Profile.php
+++ User_Profile.php
@@ -5,7 +5,110 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<body><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account Information</title>
+    <link rel="stylesheet" href="css\User_Profile.css">
+    <link rel="stylesheet" href="css\Navbar.css">
+
+</head>
 <body>
-    
+<div class="container">
+    <h1>Account Information</h1>
+    <div class="account">
+        <form id="accountForm">
+            
+            <label for="email" id="label">Name:</label>
+            <input type="text" readonly>
+            <button type="button" id="editBtn">Edit</button>
+            <button type="button" id="saveBtn" style="display: none;">Save</button><br><br>
+
+            <!-- Include similar structure for other fields... -->
+
+        </form>
+    </div>
+    <div class="account">
+        <form id="accountForm">
+
+            <label for="email" id="label">Email:</label>
+            <input type="text" readonly>
+            <button type="button" id="editBtn">Edit</button>
+            <button type="button" id="saveBtn" style="display: none;">Save</button><br><br>
+
+            <!-- Include similar structure for other fields... -->
+
+        </form>
+    </div>
+    <div class="account">
+        <form id="accountForm">
+
+            <label for="email"id="label">Password:</label>
+            <input type="text" readonly>
+            <button type="button" id="editBtn">Edit</button>
+            <button type="button" id="saveBtn" style="display: none;">Save</button><br><br>
+
+            <!-- Include similar structure for other fields... -->
+
+        </form>
+    </div>
+    <div class="account">
+        <form id="accountForm">
+
+            <label for="email" id="label">Gender:</label>
+            <input type="text" readonly>
+            <button type="button" id="editBtn">Edit</button>
+            <button type="button" id="saveBtn" style="display: none;">Save</button><br><br>
+
+            <!-- Include similar structure for other fields... -->
+
+        </form>
+    </div>
+    <div class="account">
+        <form id="accountForm">
+
+            <label for="email" id="label">Codeforces:</label>
+            <input type="text" readonly>
+            <button type="button" id="editBtn">Edit</button>
+            <button type="button" id="saveBtn" style="display: none;">Save</button><br><br>
+
+            <!-- Include similar structure for other fields... -->
+
+        </form>
+    </div>
+    <div class="account">
+        <form id="accountForm">
+
+            <label for="email" id="label">LinkedIn:</label>
+            <input type="text" readonly>
+            <button type="button" id="editBtn">Edit</button>
+            <button type="button" id="saveBtn" style="display: none;">Save</button><br><br>
+
+            <!-- Include similar structure for other fields... -->
+
+        </form>
+    </div>
+    <div class="account">
+        <form >
+            <button id="hiddenBtn">Hidden Button</button>
+
+        </form>
+    </div>
+    <div class="account">
+        <form >
+
+            <button type="button" id="log-out">Log out</button>
+            <!-- Include similar structure for other fields... -->
+
+        </form>
+    </div>
+</div>
+
+<script src="js\User_Profile.js"></script>
+</body>
+</html>
+
 </body>
 </html>
--- login.php
+++ login.php
@@ -29,7 +29,7 @@ require_once "includes/login_view.inc.php";
                 <div class="input-field">
                     <input type="password" name="pwd" placeholder="Password">
                 </div>
-                <button type="submit" class="signup-button">LOGIN</button>
+                <button type="submit" class="signup-button" id="log-in">LOGIN</button>
             </form>
             <p class="login-link">
                 <strong>Don’t have an account? </strong>
@@ -40,7 +40,7 @@ require_once "includes/login_view.inc.php";
             <?php check_login_errors(); ?>
         </div>
     </div>
-
+    <script src="js\User_Profile.js"></script>
 </body>
 
 </html>
