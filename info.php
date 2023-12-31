a3baf6c778bc4e5dc27a2b78136d5b729d01a17f
AnasBendar <anasbendary33@gmail.com>
2023-12-31 12:03:52 +0300

    uplode for cart and info

---
+++ info.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Course Page</title>
+<link rel="stylesheet" href="css/info.css">
+</head>
+<body>
+
+    <div id="main-container">
+        <div id="course-info">
+            <div id="course-details">
+                <h1 id="course-name">traning Name1</h1>
+                <p>Coach: khalid</p>
+                <p id="course-info-text">Course Information: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>Date of Download: January 31, 2023</p>
+            </div>
+            <div id="rectangle-section">
+                <p>Topics: HTML-CSS-JS</p>
+            </div>
+        </div>
+        <div id="sidebar">
+            <h1 id="course-name">traning Name 1</h1>
+            <a href="#" class="button" onclick="addCard()">Add Card</a>
+            <a href="#" class="button" onclick="showProblem()">Show Problem</a>
+        </div>
+    </div>
+    
+
+</body>
+</html>
